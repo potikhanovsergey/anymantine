16fd074b50c1936976617ca5a6a5b93b6993a3a9
Darya Syomina <d.semina10@gmail.com>
2023-03-28 16:24:58 +0300

    rewrite anchor

--- src/design-system/Atoms/AtomAnchor.tsx
+++ src/design-system/Atoms/AtomAnchor.tsx
@@ -3,75 +3,72 @@ import { IconDownload, IconHeart, IconSettings } from "@tabler/icons-react"
 import getActionIconConfigurator from "src/design-system/Configurator/configurators/action-icon"
 import ItemPreview from "src/design-system/ItemPreview"
 import { DesignTheme } from "src/state/design-system"
+import ComponentDocs, { ComponentDocsProps } from "../ComponentDocs"
 import getAnchorConfigurator from "../Configurator/configurators/anchor"
 import Example from "../Tokens/Example"
+
+const anchorComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
+  title: "Anchor",
+  description: `Anchors(links) allow users to navigate to a different location. They can be presented inside a
+  paragraph or as standalone text.`,
+  examples: [
+    {
+      title: "Size",
+      description: `Anchors come in five different sizes: extra-small, small, medium, large, and extra-large. Use the sizes sparingly; they should be used to create a hierarchy of importance within the page.`,
+      children: (
+        <Stack w="75%">
+          {["xs", "sm", "md", "lg", "xl"].map((size) => (
+            <Group w="100%" position="apart" key={size}>
+              <Text color="dimmed">{size}</Text>
+              <Anchor tabIndex={-1} size={size}>
+                Link
+              </Anchor>
+            </Group>
+          ))}
+        </Stack>
+      ),
+    },
+    {
+      title: "Underline",
+      description: `Underlined variant should be used to call attention to the link. Its no-underline appearance is optimal for when the underlined variant is too overwhelming, such as in blocks of text with several references linked throughout.`,
+      children: (
+        <Group>
+          {["underline", "no underline"].map((variant) => (
+            <Stack key={variant}>
+              <Text color="dimmed">{variant}</Text>
+              <Anchor tabIndex={-1} underline={variant === "underline"}>
+                Link
+              </Anchor>
+            </Stack>
+          ))}
+        </Group>
+      ),
+    },
+  ],
+}
 
 const AtomAnchor = () => {
   const anchorConfigurator = getAnchorConfigurator(DesignTheme.peek())
   return (
-    <Stack spacing={40}>
-      <div>
-        <Title order={1} mb="md">
-          Anchor
-        </Title>
-        <Text maw="75%">
-          Links allow users to navigate to a different location. They can be presented inside a
-          paragraph or as standalone text.
-        </Text>
-      </div>
-      <ItemPreview
-        configurator={anchorConfigurator}
-        title="Anchor"
-        mantineLink="https://mantine.dev/core/anchor"
-      >
-        <Group>
-          <Anchor color="grape" size="xl">
-            Link
-          </Anchor>
-          <Anchor color="green" underline={false}>
-            Link
-          </Anchor>
-          <Anchor strikethrough={true}>Link</Anchor>
-        </Group>
-      </ItemPreview>
-      <div>
-        <Title order={2} mb="md">
-          Options
-        </Title>
-        <Stack spacing={40}>
-          <Example
-            title="Size"
-            description="Anchors come in five different sizes: extra-small, small, medium, large, and extra-large. Use the sizes sparingly; they should be used to create a hierarchy of importance within the page."
-          >
-            <Stack w="75%">
-              {["xs", "sm", "md", "lg", "xl"].map((size) => (
-                <Group w="100%" position="apart" key={size}>
-                  <Text color="dimmed">{size}</Text>
-                  <Anchor tabIndex={-1} size={size}>
-                    Link
-                  </Anchor>
-                </Group>
-              ))}
-            </Stack>
-          </Example>
-          <Example
-            title="Underline"
-            description="Underlined variant should be used to call attention to the link. Its no-underline appearance is optimal for when the underlined variant is too overwhelming, such as in blocks of text with several references linked throughout."
-          >
-            <Group>
-              {["underline", "no underline"].map((variant) => (
-                <Stack key={variant}>
-                  <Text color="dimmed">{variant}</Text>
-                  <Anchor tabIndex={-1} underline={variant === "underline"}>
-                    Link
-                  </Anchor>
-                </Stack>
-              ))}
-            </Group>
-          </Example>
-        </Stack>
-      </div>
-    </Stack>
+    <ComponentDocs
+      {...anchorComponentDocsProps}
+      preview={{
+        title: "Anchor",
+        mantineLink: "https://mantine.dev/core/anchor",
+        configurator: anchorConfigurator,
+        children: (
+          <Group>
+            <Anchor color="grape" size="xl">
+              Link
+            </Anchor>
+            <Anchor color="green" underline={false}>
+              Link
+            </Anchor>
+            <Anchor strikethrough={true}>Link</Anchor>
+          </Group>
+        ),
+      }}
+    />
   )
 }
 
