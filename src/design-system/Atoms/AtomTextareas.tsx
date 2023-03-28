9adffbfea70a6eb1cc8c31943fec2da64cafd1bc
sp <potikhanovsergey@gmail.com>
2023-03-28 22:57:23 +0300

    Textarea ComponentDocs

--- src/design-system/Atoms/AtomTextareas.tsx
+++ src/design-system/Atoms/AtomTextareas.tsx
@@ -1,22 +1,60 @@
-import { SimpleGrid, Textarea } from "@mantine/core"
+import { SimpleGrid, Textarea, Text, Stack } from "@mantine/core"
 import getTextareaConfigurator from "src/design-system/Configurator/configurators/textarea"
-import ItemPreview from "src/design-system/ItemPreview"
 import { DesignTheme } from "src/state/design-system"
+import ComponentDocs, { ComponentDocsProps } from "../ComponentDocs"
+
+const textareaDocsProps: Omit<ComponentDocsProps, "preview"> = {
+  title: "Textarea",
+  description: `A text area component lets a user input a longer amount of text than a standard text field. `,
+  examples: [
+    {
+      title: "Autosize",
+      description: (
+        <Text>
+          With <strong>autosize</strong> prop set to true textarea&apos;s height will grow until
+          maxRows are reached or indefinitely if maxRows not set.
+        </Text>
+      ),
+      children: (
+        <Stack>
+          <Textarea
+            placeholder="Autosize with no rows limit"
+            label="Autosize with no rows limit"
+            autosize
+            minRows={2}
+          />
+
+          <Textarea
+            label="Autosize with 4 rows max"
+            placeholder="Autosize with 4 rows max"
+            autosize
+            minRows={2}
+            maxRows={4}
+          />
+        </Stack>
+      ),
+    },
+  ],
+}
 
 const AtomTextareas = () => {
   const textareaConfigurator = getTextareaConfigurator(DesignTheme.peek())
   return (
-    <ItemPreview
-      configurator={textareaConfigurator}
-      title="Textarea"
-      mantineLink="https://mantine.dev/core/textarea"
-    >
-      <SimpleGrid cols={2}>
-        <Textarea label="Label" placeholder="Placeholder..." />
-        <Textarea label="Label" defaultValue="Wrong value..." error="Error..." />
-        <Textarea label="Label" description="Description..." placeholder="Placeholder..." />
-      </SimpleGrid>
-    </ItemPreview>
+    <ComponentDocs
+      {...textareaDocsProps}
+      preview={{
+        title: "Textarea",
+        configurator: textareaConfigurator,
+        mantineLink: "https://mantine.dev/core/textarea",
+        children: (
+          <SimpleGrid cols={2}>
+            <Textarea label="Label" placeholder="Placeholder..." />
+            <Textarea label="Label" defaultValue="Wrong value..." error="Error..." />
+            <Textarea label="Label" description="Description..." placeholder="Placeholder..." />
+          </SimpleGrid>
+        ),
+      }}
+    />
   )
 }
 
