c7aa6a42e2b4202acef9a47baf7b861c84c714bd
sp <potikhanovsergey@gmail.com>
2023-03-31 23:11:56 +0300

    Add text for palette

--- src/design-system/Tokens/Colors/ColorsPage.tsx
+++ src/design-system/Tokens/Colors/ColorsPage.tsx
@@ -1,8 +1,9 @@
-import { Stack } from "@mantine/core"
+import { Stack, Text } from "@mantine/core"
 import PageTitle from "src/design-system/layout/PageTitle"
 import PrimaryColor from "./PrimaryColor"
 import ThemeColors from "./ThemeColors"
 import PageSubtitle from "src/design-system/layout/PageSubtitle"
+import Link from "src/core/components/atoms/Link"
 
 export interface ColorsPageProps {
   colors: string[]
@@ -12,9 +13,18 @@ const ColorsPage = ({ colors }: ColorsPageProps) => {
   return (
     <>
       <PageTitle>Colors</PageTitle>
+
       <Stack spacing={64}>
         <div>
-          <PageSubtitle>Pallete</PageSubtitle>
+          <PageSubtitle mb="sm">Pallete</PageSubtitle>
+          <Text mb="md">
+            Usually, Animantine themes rewrite several colors from the standard Mantine palette. A
+            list of all the colors available can be found at{" "}
+            <Link href="https://mantine.dev/theming/colors/" target="_blank">
+              Mantine Colors
+            </Link>
+            . You can also always add new colors or rewrite existing ones.
+          </Text>
           <ThemeColors colors={colors} />
         </div>
         <div>
