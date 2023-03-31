ae5131ff1290f076073a7482c1281e63de267698
sp <potikhanovsergey@gmail.com>
2023-03-31 16:03:08 +0300

    Refactor atom radio

--- src/design-system/Atoms/AtomRadio.tsx
+++ src/design-system/Atoms/AtomRadio.tsx
@@ -1,4 +1,4 @@
-import { Group, Stack, Radio, Text } from "@mantine/core"
+import { Group, Stack, Radio, Text, Center } from "@mantine/core"
 import getRadioConfigurator from "src/design-system/Configurator/configurators/radio"
 import { DesignTheme } from "src/state/design-system"
 import ComponentDocs, { ComponentDocsProps } from "../ComponentDocs"
@@ -13,11 +13,13 @@ const radioComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
       title: "Label",
       description: `Radio buttons should always have a label. When the label is not defined, a radio button becomes standalone. Standalone radio buttons are only used when their connection to other components is clear and they give sufficient context — for example, in application panels.`,
       children: (
-        <Group>
-          {["with label", "alone"].map((variant) => (
+        <Group position="center">
+          {["labeled", "standalone"].map((variant) => (
             <Stack key={variant}>
-              <Text color="dimmed">{variant}</Text>
-              <Radio value="kiwi" label={variant === "with label" && "Kiwi"} />{" "}
+              <Text color="dimmed" tt="capitalize">
+                {variant}
+              </Text>
+              <Radio value="kiwi" label={variant === "labeled" && "Kiwi"} />{" "}
             </Stack>
           ))}
         </Group>
@@ -27,7 +29,7 @@ const radioComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
       title: "Label position",
       description: `You can use different label positions depending on the layout where you use radio buttons.`,
       children: (
-        <Stack>
+        <Stack align="center">
           {["left", "right"].map((position) => (
             <Radio
               value="kiwi"
@@ -43,9 +45,9 @@ const radioComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
       title: "Size",
       description: `Radio buttons come in five different sizes: extra-small, small, medium, large, and extra-large. Use the sizes sparingly; they should be used to create a hierarchy of importance within the page.`,
       children: (
-        <Stack w="75%">
+        <Stack align="center">
           {["xs", "sm", "md", "lg", "xl"].map((size) => (
-            <Group w="100%" position="apart" key={size}>
+            <Group w="100%" position="center" key={size}>
               <Text color="dimmed">{size}</Text>
               <Radio value="kiwi" label="Kiwi" size={size} />
             </Group>
@@ -57,9 +59,9 @@ const radioComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
       title: "Disabled",
       description: `A radio button in a disabled state shows that a selection exists, but is not available in that circumstance. This can be used to maintain layout continuity and communicate that an action may become available later.`,
       children: (
-        <Group>
-          {["not selected", "selected"].map((variant) => (
-            <Radio key={variant} disabled label="Kiwi" readOnly checked={variant === "selected"} />
+        <Group position="center">
+          {[false, true].map((checked) => (
+            <Radio key={checked + ""} disabled label="Kiwi" readOnly checked={checked} />
           ))}
         </Group>
       ),
@@ -68,15 +70,9 @@ const radioComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
       title: "Error",
       description: `Radio buttons can be marked as having an error to show that a selection needs to be made in order to move forward, or that a selection that was made is invalid. For example, in a form that requires a user to acknowledge legal terms before proceeding, the checkbox would show an unchecked error to communicate that it needs to be selected.`,
       children: (
-        <Group>
-          {["not selected", "selected"].map((variant) => (
-            <Radio
-              key={variant}
-              error="Error"
-              label="Kiwi"
-              readOnly
-              checked={variant === "selected"}
-            />
+        <Group position="center">
+          {[false, true].map((checked) => (
+            <Radio key={checked + ""} error="Error" label="Kiwi" readOnly checked={checked} />
           ))}
         </Group>
       ),
@@ -85,20 +81,22 @@ const radioComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
       title: "Required",
       description: `Radio buttons group can be marked as required to show that a selection needs to be made in order to move forward. For example, in a form that requires a user to acknowledge legal terms before proceeding, the checkbox would show an unchecked error to communicate that it needs to be selected. WithAsterisk prop do the same thing but without logic.`,
       children: (
-        <Radio.Group
-          name="favoriteFruitRequired"
-          label="Select your favorite fruit"
-          description="This is anonymous"
-          withAsterisk
-          defaultValue="mango"
-        >
-          <Group mt="xs">
-            <Radio value="kiwi" label="Kiwi" />
-            <Radio value="mango" label="Mango" />
-            <Radio value="apple" label="Apple" />
-            <Radio value="banana" label="Banana" />
-          </Group>
-        </Radio.Group>
+        <Center>
+          <Radio.Group
+            name="favoriteFruitRequired"
+            label="Select your favorite fruit"
+            description="This is anonymous"
+            withAsterisk
+            defaultValue="mango"
+          >
+            <Group mt="xs">
+              <Radio value="kiwi" label="Kiwi" />
+              <Radio value="mango" label="Mango" />
+              <Radio value="apple" label="Apple" />
+              <Radio value="banana" label="Banana" />
+            </Group>
+          </Radio.Group>
+        </Center>
       ),
     },
   ],
