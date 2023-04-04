53117add498e49c31d2eaf8f21b9d652fa71fe52
sp <potikhanovsergey@gmail.com>
2023-04-04 17:25:11 +0300

    Code example in UsagePage now depends on selected theme

--- src/design-system/Atoms/AtomPaper/index.tsx
+++ src/design-system/Atoms/AtomPaper/index.tsx
@@ -18,10 +18,12 @@ const paperComponentDocsProps: Omit<ComponentDocsProps, "preview"> = {
       title: "Shadow",
       description: `The different size of the shadow changes the perception of the card: it is necessary to select it according to the overall composition and how important the information is. A large shadow will draw more attention.`,
       children: (
-        <Stack align="center">
+        <Stack>
           {["xs", "sm", "md", "lg", "xl"].map((size) => (
             <Group w="100%" position="apart" key={size} noWrap>
-              <Text color="dimmed">{size}</Text>
+              <Text miw={40} color="dimmed">
+                {size}
+              </Text>
               <Paper withBorder={false} shadow={size}>
                 {paperDataShortMock}
               </Paper>
